fe4d36ec396c15cd3091b494daf8995f5cafb217
鲍剑 <jian_bao@outlook.com>
2017-06-02 00:54:22 +0800

    change pattern

--- demo/patterns/patterns.js
+++ demo/patterns/patterns.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports  = {
-    DatePicker: fs.readFileSync(path.join(__dirname, './test.js'), 'utf-8')
+    DatePicker: fs.readFileSync(path.join(__dirname, './DatePicker/index.js'), 'utf-8')
 }
