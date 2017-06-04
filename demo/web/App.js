8f3f794f82d83e292f796e237ea6e1bfe63d7750
鲍剑 <jian_bao@outlook.com>
2017-06-04 18:13:12 +0800

    add mark

--- demo/web/App.js
+++ demo/web/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                HomePage
+                Created By Baojian & Xieyixuan
             </div>
         );
     }
