b783755f7217deafef3859f2d10a0ee8cb0a5c5e
鲍剑 <jian_bao@outlook.com>
2017-06-07 00:30:44 +0800

    change datepicker

---
+++ src/components/DatePicker/DateInput/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.less';
+
+class DateInput extends Component {
+    render() {
+        const { date, format} = this.props;
+        return (
+            <ul className='DateInput'>
+                <li>
+                    <span className='icon-calendar'></span>
+                </li>
+                <li>
+                    <input type='text' placeholder='Date' />
+                </li>
+            </ul>
+        );
+    }
+}
+
+DateInput.propTypes = {
+    format: PropTypes.string
+};
+
+export default DateInput;
--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,3 +1,37 @@
-import DatePicker from './Calendar';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import DateInput from './DateInput';
+import Tooltip from '../Tooltip';
+import Calendar from '../Calendar';
+
+class DatePicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: null
+        };
+    }
+
+    render() {
+        const { format } = this.props;
+        const { date } = this.state;
+        return (
+            <Tooltip trigger='hover' overlay={<Calendar />}>
+                <div>
+                    <DateInput format={format} date={date} />
+                </div>
+            </Tooltip>
+        );
+    }
+}
+
+DatePicker.defaultProps = {
+    format: 'DD MMM YYYY'
+}
+
+DatePicker.propTypes = {
+    formt: PropTypes.string
+};
 
 export default DatePicker;
